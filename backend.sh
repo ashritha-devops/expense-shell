9d098b80a806e811475713eb5deaf4c050fdef1d
ashritha <ashritha.achala90@gmail.com>
2024-03-23 14:59:35 -0400

    new test files1

--- backend.sh
+++ backend.sh
@@ -1,21 +1,56 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd expense
-cp backend.service /etc/systemd/system/backend.service
+source common.sh
 
-rm -rf /app
-mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
-unzip /tmp/backend.zip
-cd /app
-npm install
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+mysql_root_password=$1
+app_dir=/app
+component=backend
 
-dnf install mysql -y
+# If password is not provided then we will exit
+if [ -z "${mysql_root_password}" ]; then
+  echo Input Password is missing.
+  exit 1
+fi
 
-mysql -h 34.201.109.92 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+Print_Task_Heading "Disable default NodeJS Version Module"
+dnf module disable nodejs -y &>>$LOG
+Check_Status $? 
+
+Print_Task_Heading "Enable NodeJS module for V20"
+dnf module enable nodejs:20 -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Install NodeJS"
+dnf install nodejs -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Adding Application User"
+id expense &>>$LOG
+if [ $? -ne 0 ]; then
+  useradd expense &>>$LOG
+fi
+Check_Status $?
+
+Print_Task_Heading "Copy Backend Service file"
+cp backend.service /etc/systemd/system/backend.service &>>$LOG
+Check_Status $?
+
+App_PreReq
+
+Print_Task_Heading "Download NodeJS Dependencies"
+cd /app &>>$LOG
+npm install &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Start Backend Service"
+systemctl daemon-reload &>>$LOG
+systemctl enable backend &>>$LOG
+systemctl start backend &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Install MySQL Client"
+dnf install mysql -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Load Schema"
+mysql -h mysql-dev.rdevops6a.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
+Check_Status $?
